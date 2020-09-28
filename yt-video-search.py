9bddbfee07229b42c2bf41bc0a474f54397ef5e2
Gaurav <gaurav.2897@gmail.com>
2020-09-28 19:51:28 +0530

    Update yt-video-search.py

--- yt-video-search.py
+++ yt-video-search.py
@@ -1,5 +1,5 @@
 from youtube_search import YoutubeSearch
-
+import subprocess
 results = YoutubeSearch('expert jatt', max_results=10).to_json()
 
 print(results)
@@ -9,6 +9,8 @@ print(results)
 ########################################
 
 results = YoutubeSearch('search terms', max_results=10).to_dict()
+
+proc = subprocess.Popen(["chrome", "https://youtu.be/uBpwDnYuVO0"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 print(results)
 # returns a dictionary
